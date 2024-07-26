d039cb64ec3c3ad34fc959f3526bb9043ee3608c
Harsha7668 <132264211+Harsha7668@users.noreply.github.com>
2024-07-26 06:22:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from pyrogram import Client, filters
-from utils import check_user, multi_rec, getChannels
+from utils import check_user, multi_rec, getChannels, webdl_command_handler
+
+from datetime import datetime, timedelta
+
+
 
 app = Client(
     "MU3Link",
@@ -22,6 +26,62 @@ def multirec_handler(app, message):
         return
 
     multi_rec(app, message)
+
+
+
+
+# Function to handle the /webdl command
+@app.on_message(filters.incoming & filters.command(['webdl']) & filters.text)
+def webdl_handler(app, message):
+    auth_user = check_user(message)
+    if auth_user is None:
+        return
+
+    cmd_parts = message.text.split()
+    channel = None
+    start_time = None
+    end_time = None
+    title = None
+
+    for i, part in enumerate(cmd_parts):
+        if part == '-c':
+            channel = cmd_parts[i + 1]
+        elif part == '-ss':
+            start_time = cmd_parts[i + 1]
+        elif part == '-to':
+            end_time = cmd_parts[i + 1]
+        elif part == '-title':
+            title = " ".join(cmd_parts[i + 1:])
+            break
+
+    if not channel or not start_time or not end_time or not title:
+        message.reply_text("<b>Syntax: </b>`/webdl -c [channel] -ss [start_time] -to [end_time] -title [title]`")
+        return
+
+    # Convert start_time and end_time to datetime objects
+    try:
+        start_time = datetime.strptime(start_time, "%d/%m/%Y+%H:%M:%S")
+        end_time = datetime.strptime(end_time, "%d/%m/%Y+%H:%M:%S")
+    except ValueError:
+        message.reply_text("<b>Error:</b> Invalid date format. Use `dd/mm/yyyy+HH:MM:SS`.")
+        return
+
+    # Check if the start_time is within the last 7 days
+    if start_time < datetime.now() - timedelta(days=7):
+        message.reply_text("You can only record sessions within the last 7 days.")
+        return
+
+    # Check if the end time is after the start time
+    if end_time <= start_time:
+        message.reply_text("End time must be after start time.")
+        return
+
+    # Call the recording function
+    webdl_command_handler(app, message, channel, start_time, end_time, title)
+
+
+
+    
 
 @app.on_message(filters.incoming & filters.command(['channels']) & filters.text)
 def show_channels_handler(app, message):
